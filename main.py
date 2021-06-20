2941f765ffb1d4d7fd9775f549028684aa12d92e
jaswanthraj <64518811+dev-jaswanthraj@users.noreply.github.com>
2021-06-21 00:48:52 +0530

    Added add function with two attributes.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 def func():
     print("Hello Git")
     return
+def add(a, b):
+    return a+b
+
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+    result = add(a, b)
+    print(result)
